02cada101d97e0c39dae746c100b74acbebb80fb
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-01-18 14:18:03 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -8,9 +8,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [metosin/ring-swagger "0.22.3"]
                  [cheshire "5.5.0"]
-                 [ronda/routing "0.2.7"]]
+                 [ronda/routing "0.2.8"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]
-                                  [ronda/routing-bidi "0.1.2"]
+                                  [ronda/routing-bidi "0.1.3"]
                                   [org.clojure/math.combinatorics "0.1.1"]
                                   [joda-time "2.9.1"]]
                    :plugins [[lein-midje "3.1.3"]]}
